bc4c04c859a965eeef8013c0786732f05542c046
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2019-12-19 20:10:39 +0330

    parsing form bodies

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -6,4 +6,25 @@ router.get("/", (req: Request, res: Response) => {
   res.send("hi there");
 });
 
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send(email + password);
+});
+
 export { router };
